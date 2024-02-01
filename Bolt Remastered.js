910881e25dc00db7537302ae3792876840ecb425
ChrisM90916 <02christopher.melbow@gulllakecs.org>
2024-02-01 08:01:28 -0500

    Start maze

---
+++ Bolt Remastered.js
@@ -0,0 +1,11 @@
+// Team Members: Christopher Melbow & Ethan Mingerink 
+// Name: Christopher Melbow
+// Date: 1.22.2024
+// Program: Maze
+// Bolt ID: B0D0
+
+async function startProgram() {
+	// start maze 
+	await scrollMatrixText('Start',{ r:255,g:0,b:255}, 30, true)
+	await speak ("Start Maze", true);
+	setMainLed ({ r:255,g:0,b:255});
